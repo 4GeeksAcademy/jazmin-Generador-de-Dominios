f152d5a0de79f5aa567ae2a088134e15a386f5d8
JazminM08 <122406036+JazminM08@users.noreply.github.com>
2025-02-19 21:14:48 -0600

    Código ejercicio
    Se agrega el código del ejercicio

--- src/app.js
+++ src/app.js
@@ -9,3 +9,22 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronouns = ['the', 'our'];
+let adjs = ['great', 'big'];
+let nouns = ['jogger', 'racoon'];
+
+let mezclaArrays = [];
+let count = 1;
+
+for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+        for (let noun of nouns) {
+            let links = `${pronoun}${adj}${noun}.com`;
+            mezclaArrays.push(`${count}.${links}`);
+            count++;
+        }
+    }
+}
+
+console.log(mezclaArrays.join("\n"));
